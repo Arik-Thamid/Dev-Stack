69c34c9aa42b64dea09c4590b49b8ce59dbb7263
Arik <arik.me229@gmail.com>
2026-09-12 17:09:34 +0600

    Create navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,35 @@
+import Logo from '../assets/logo-text.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <>
+    <nav className="sticky top-0 bg-white border-b border-gray-100">
+     <div className="container mx-auto">
+        <div className="flex justify-between items-center py-4">
+
+        <img src={Logo} alt="logo" />
+
+        <ul className="flex items-center gap-7 text-sm">
+            <li><a className="text-pink-500" href="#">Home</a></li>
+            <li><a href="#">Technologies</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+
+        <div className="flex items-center gap-4">
+            <button className="text-sm">Sign In</button>
+
+            <button className="bg-[#D91B7E] text-[#FFFFFF] text-sm px-5 py-2 rounded-full">
+            Sign Up
+            </button>
+        </div>
+
+         </div>
+     </div>
+    </nav>
+
+   </>
   )
 }
 
