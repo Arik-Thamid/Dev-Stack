1a093041f2ee5f99fcf95673a075bea465183685
Arik <arik.me229@gmail.com>
2026-09-12 17:24:58 +0600

    correct app and correct json file also

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { Suspense, useState } from "react"
+import Banner from "./components/Banner"
+import Navbar from "./components/Navbar"
+import type { DataType } from "./types/type"
+import Technology from "./components/Technologies/Technology"
+import Footer from "./components/Footer"
 
-const App = () => {
+
+const dataFetch = async (): Promise<DataType[]> => {
+      const res = await fetch('/data.json')
+      const data = await res.json()
+      return data
+  }
+
+export default function App() {
+    const [dataProps] = useState(()=> dataFetch())
+    const [stackData, setStackData] = useState<DataType[]>([])
+
   return (
-    <div>App</div>
+    <div>
+      
+      <Navbar/>
+      <Banner/>
+      <Suspense fallback={<h1 className="container mx-auto text-2xl m-10">Loading technologies...</h1>}>
+        <Technology dataProps={dataProps} stackData={stackData} setStackData={setStackData} />
+      </Suspense>
+      <Footer/>
+
+      
+    </div>
   )
 }
-
-export default App
-
